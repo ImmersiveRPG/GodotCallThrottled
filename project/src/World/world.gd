282cfdf4a0aae3d7f5fb4cdce4cad6086ce3c064
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2023-06-27 14:02:43 -0700

    Added ball count to title bar

--- project/src/World/world.gd
+++ project/src/World/world.gd
@@ -16,7 +16,8 @@ func _ready() -> void:
 
 func _on_fps_timer_timeout() -> void:
 	var fps := Engine.get_frames_per_second()
-	var title := "FPS: %s" % [fps]
+	var count := _ball_holder.get_child_count()
+	var title := "FPS: %s, Balls: %s" % [fps, count]
 	self.get_window().set_title(title)
 
 func _on_button_spawn_balls_pressed() -> void:
@@ -39,6 +40,23 @@ func _on_button_spawn_balls_pressed() -> void:
 	var used := after - before
 	print("Blocked for msecs: %s" % [used])
 
+func _on_button_spawn_balls_deferred_pressed() -> void:
+	var cb := func():
+		# Add ball
+		var ball := _ball_scene.instantiate()
+		_ball_holder.add_child(ball)
+
+		# Give ball random position around center
+		const r := 25.0
+		ball.transform.origin = Vector3(
+			randf_range(-r, r),
+			3.0,
+			randf_range(-r, r),
+		)
+
+	for n in 500:
+		cb.call_deferred()
+
 func _on_button_spawn_balls_throttled_pressed() -> void:
 	var cb := func():
 		# Add ball
@@ -56,23 +74,6 @@ func _on_button_spawn_balls_throttled_pressed() -> void:
 	for n in 500:
 		Throttler.call_throttled(cb)
 
-
-func _on_button_spawn_balls_deferred_pressed() -> void:
-	var cb := func():
-		# Add ball
-		var ball := _ball_scene.instantiate()
-		_ball_holder.add_child(ball)
-
-		# Give ball random position around center
-		const r := 25.0
-		ball.transform.origin = Vector3(
-			randf_range(-r, r),
-			3.0,
-			randf_range(-r, r),
-		)
-
-	for n in 500:
-		cb.call_deferred()
 
 
 func _on_button_remove_all_balls_pressed() -> void:
