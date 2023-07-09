8d63e8ba8b2d260a6dd197c3d26c37b7e46b9ca5
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2023-07-08 23:31:37 -0700

    Removed duplicate example code

--- project/src/World/world.gd
+++ project/src/World/world.gd
@@ -63,27 +63,8 @@ func _on_button_spawn_balls_deferred_pressed() -> void:
 			randf_range(-r, r),
 		)
 
-	var cb_throt := func():
-		#print("        cb_throt a: %s, frame: %s" % [Time.get_ticks_msec(), self.get_tree().get_frame()])
-		#OS.delay_msec(1000)
-		# Add ball
-		var ball := _ball_scene.instantiate()
-		_ball_holder.add_child(ball)
-
-		# Give ball random position around center
-		const r := 25.0
-		ball.transform.origin = Vector3(
-			randf_range(-r, r),
-			3.0,
-			randf_range(-r, r),
-		)
-		#print("        cb_throt b: %s, frame: %s" % [Time.get_ticks_msec(), self.get_tree().get_frame()])
-
 	for n in 500:
 		cb_defer.call_deferred()
-
-	for n in 500:
-		GodotCallThrottled.call_throttled(cb_throt)
 
 	#print("        button end: %s, frame: %s" % [Time.get_ticks_msec(), self.get_tree().get_frame()])
 
