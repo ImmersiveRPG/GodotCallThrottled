bbb298742a9a8a4d16b1c5d6872196bf41375d9c
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2024-01-09 11:37:18 -0800

    Moved tool header below copyright header.

--- addons/GodotCallThrottled/godot_call_throttled.gd
+++ addons/GodotCallThrottled/godot_call_throttled.gd
@@ -1,8 +1,8 @@
-@tool
 # Copyright (c) 2022-2024 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 # This file is licensed under the MIT License
 # https://github.com/ImmersiveRPG/GodotCallThrottled
 
+@tool
 extends EditorPlugin
 
 # Get the name and paths of all the autoloads
