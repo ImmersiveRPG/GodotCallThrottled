01612f0d62ec3939cd74e2ff7aa2fafd74148777
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2024-01-09 11:35:26 -0800

    Updated copyright dates.

--- addons/GodotCallThrottled/Singletons/CallThrottled.gd
+++ addons/GodotCallThrottled/Singletons/CallThrottled.gd
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+# Copyright (c) 2022-2024 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 # This file is licensed under the MIT License
 # https://github.com/ImmersiveRPG/GodotCallThrottled
 
--- addons/GodotCallThrottled/godot_call_throttled.gd
+++ addons/GodotCallThrottled/godot_call_throttled.gd
@@ -1,5 +1,5 @@
 @tool
-# Copyright (c) 2023 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+# Copyright (c) 2022-2024 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 # This file is licensed under the MIT License
 # https://github.com/ImmersiveRPG/GodotCallThrottled
 
